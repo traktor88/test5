1c3f4483a0171d26087f4183c0c464cd132f5827
traktor88 <14traktor88@gmail.com>
2010-03-09 04:14:50 +0000

    Added the signals module.

--- hashtags/signals.py
+++ hashtags/signals.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-#
 # Copyright (c) 2010 Guilherme Gondim and contributors
 #
 # This file is part of Django Hashtags.
@@ -8,58 +6,44 @@
 # General Public License version 3 (LGPLv3) as published by the Free
 # Software Foundation. See the file README for copying conditions.
 
-"""
-Signals relating to hashtags.
-"""
-
+from django.contrib.contenttypes.models import ContentType
 from django.dispatch import Signal
-from hashtags.utils import link_hashtags_to_model
+from django.db.utils import IntegrityError
+from hashtags.models import Hashtag, HashtaggedItem
+from hashtags.utils import hashtag_pattern
 
-# A post-save signal hook to you connect function handlers to work with
-# hashtagged model fields.
 hashtagged_model_was_saved = Signal(providing_args=['hashtagged_field_list'])
 
-def parse_fields_looking_for_hashtags(sender, instance, hashtagged_field_list=None,
-                                      **kwargs):
-    """
-    A function handler to work with ``hashtagged_model_was_saved`` signal. This
-    handler parse a list of model fields looking for hashtags to be
-    linked/related with the instance in question.
-
-    Usage example::
-
-        # You need connect ``parse_fields_looking_for_hashtags`` on
-        # ``hashtagged_model_was_saved`` only one time.
-        from hashtags.signals import (hashtagged_model_was_saved,
-                                      parse_fields_looking_for_hashtags)
-        hashtagged_model_was_saved.connect(parse_fields_looking_for_hashtags)
-
-    Connecting your models that you want track hashtags (FlatPage example)::
-
-        from django.contrib.flatpages.models import FlatPage
-        from django.db.models.signals import post_save
-
-        # connect hashtagged_model_was_saved signal to post_save
-        def post_save_handler(sender, instance, **kwargs):
-            hashtagged_model_was_saved.send(sender=sender, instance=instance,
-                # put the hashtagged fields of your app here
-                hashtagged_field_list=['title', 'content']
-            )
-        post_save.connect(post_save_handler, sender=FlatPage)
-
-    Alternatively you can set ``hashtagged_field_list`` in your model as a
-    class attribute, then your ``post_save_handler`` can be:
-
-        def post_save_handler(sender, instance, **kwargs):
-            hashtagged_model_was_saved.send(sender=sender, instance=instance)
-
-    """
+def parse_a_field_looking_for_hashtags(sender, instance,
+                                       hashtagged_field_list=None, **kwargs):
     if not hashtagged_field_list:
         try:
             hashtagged_field_list = sender.hashtagged_field_list
         except AttributeError:
             return
-    text = ''
+
+    # parsing fields looking for hashtags to be linked with the instance
+    hashtag_list = []
     for field in hashtagged_field_list:
-        text += instance.__getattribute__(field) + '\n'
-    link_hashtags_to_model(text, instance)
+        for hname in hashtag_pattern.findall(instance.__getattribute__(field)):
+            hashtag = Hashtag.objects.get_or_create(name=hname)
+            hashtag_list.append(hashtag)
+
+    # unlinking instance from old hashtags and purging unused hashtags
+    instance_type = ContentType.objects.get_for_model(sender)
+    qs = HashtaggedItem.objects.exclude(hashtag__in=hashtag_list)
+    qs = qs.filter(content_type=instance_type, object_id=instance.id)
+    old_hashtag_list = [item.hashtag for item in qs]
+    for hashtag in old_hashtag_list:
+        if hashtag.hashtaggeditem_set.all().count() == 1:
+            hashtag.delete()
+        else:
+            HashtaggedItem.objects.get(hashtag=hashtag, object_id=instance.id,
+                                       content_type=instance_type).delete()
+
+    # linking instance to the new hashtags
+    for hashtag in hashtag_list:
+        try:
+            HashtaggedItem(content_object=instance, hashtag=hashtag).save()
+        except IntegrityError:
+            pass
